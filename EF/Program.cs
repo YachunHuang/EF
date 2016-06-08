b3f4790aa0c5310febf271bd09b3794f7b511e83
Yachun Huang <yachun@edetw.com>
2016-06-08 10:43:36 +0800

    Entity Framework

---
+++ EF/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new ContosoUniversityEntities())
+            {
+                foreach (var item in db.Course)
+                {
+                    Console.Write(item.Title);
+                }
+            }
+        }
+    }
+}
